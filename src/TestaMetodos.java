3ac0dcd045feb8587072645df764607c70d17ee8
Vinicius Porto <vinicius.porto@mercadolivre.com>
2021-11-04 23:45:03 -0300

    Definindo comportamento

---
+++ src/TestaMetodos.java
@@ -0,0 +1,24 @@
+public class TestaMetodos {
+
+    public static void main(String[] args) {
+
+        Conta contaDoVinicius = new Conta();
+        contaDoVinicius.saldo = 100;
+        contaDoVinicius.deposita(50);
+        System.out.println(contaDoVinicius.saldo);
+        contaDoVinicius.saca(20);
+        System.out.println(contaDoVinicius.saldo);
+
+        Conta contaDaIsabela = new Conta();
+        contaDaIsabela.deposita(1000);
+        boolean sucessoTransferencia = contaDaIsabela.transfere(300,contaDoVinicius);
+
+        if(sucessoTransferencia) {
+            System.out.println("transferencia com sucesso");
+        }else {
+            System.out.println("faltou dinheiro");
+        }
+            System.out.println(contaDaIsabela.saldo);
+            System.out.println(contaDoVinicius.saldo);
+    }
+}
---
+++ src/TesteReferencias.java
@@ -0,0 +1,33 @@
+public class TesteReferencias {
+
+    public static void main(String[] args) {
+
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 300;
+        System.out.println("saldo da primeira: " + primeiraConta.saldo);
+
+        Conta segundaConta = primeiraConta;
+        System.out.println("saldo da segunda conta: " + segundaConta.saldo);
+
+        segundaConta.saldo += 100;
+        System.out.println("saldo da segunda conta: " + segundaConta.saldo);
+
+        System.out.println(primeiraConta.saldo);
+
+
+        if(primeiraConta == segundaConta){
+            System.out.println("São a mesma conta");
+        } else {
+            System.out.println("São conta diferentes");
+        }
+
+        if(segundaConta == primeiraConta){
+            System.out.println("São a mesma conta");
+        } else {
+            System.out.println("São conta diferentes");
+        }
+
+        System.out.println(primeiraConta);
+
+    }
+}
